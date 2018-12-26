3d019c770fa5798f65bcbeb6d01264a7e1003ef8
ellf32 <46119452+ellf32@users.noreply.github.com>
2018-12-26 10:23:52 +0200

    Update config.h

--- grbl/config.h
+++ grbl/config.h
@@ -710,7 +710,7 @@
 #define PROBE_DDR       NotUsed
 #define PROBE_PIN       NotUsed
 #define PROBE_PORT      NotUsed
-#define PROBE_BIT       25  // Uno Analog Pin 5
+#define PROBE_BIT       23  // Uno Analog Pin 5
 #define PROBE_MASK      (1<<PROBE_BIT)
 
 // The LPC17xx has 6 PWM channels. Each channel has 2 pins. It can drive both pins simultaneously to the same value.
