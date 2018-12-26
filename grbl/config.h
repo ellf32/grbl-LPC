57260090c1eae949bcd0f8b32ebe8d354986b54c
ellf32 <46119452+ellf32@users.noreply.github.com>
2018-12-26 10:06:57 +0200

    Update config.h

--- grbl/config.h
+++ grbl/config.h
@@ -707,9 +707,9 @@
 #define CONTROL_INVERT_MASK   CONTROL_MASK // May be re-defined to only invert certain control pins.
 
 // Define probe switch input pin.
-#define PROBE_DDR         LPC_GPIO0->FIODIR
-#define PROBE_PIN         LPC_GPIO0->FIOPIN
-#define PROBE_PORT        LPC_GPIO0->FIOPIN
+#define PROBE_DDR         NotUsed
+#define PROBE_PIN         NotUsed
+#define PROBE_PORT        NotUsed
 #define PROBE_BIT       26  // probe = 0.26
 #define PROBE_MASK      (1<<PROBE_BIT)
 
